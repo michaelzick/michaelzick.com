8864669a3157379d7d68c479d19ccfff24035934
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-08-08 15:01:39 -0700

    style: adjust Work With Me layout

--- app/work-with-me/page.tsx
+++ app/work-with-me/page.tsx
@@ -1,20 +1,20 @@
 export default function WorkWithMe() {
   return (
     <div className="flex flex-col">
-      <section className="bg-[var(--dark-blue)] text-white px-6 md:px-8 py-24 md:py-32">
+      <section className="bg-[var(--dark-blue)] text-white px-6 md:px-8 pt-40 md:pt-56 pb-24 md:pb-32">
         <div className="max-w-screen-xl mx-auto">
-          <h1 className="text-4xl font-semibold mb-4">Work With Me</h1>
-          <h3 className="text-xl font-light">
+          <h1 className="text-[64px] font-semibold mb-6">Work With Me</h1>
+          <h3 className="text-[35px] font-light">
             “You can't go back and change the beginning, but you can start where you are and change the ending.” — C.S.
             Lewis
           </h3>
         </div>
       </section>
       <section className="px-6 md:px-8 py-24 md:py-32">
-        <div className="max-w-screen-md mx-auto space-y-6">
-          <h2 className="text-3xl font-semibold text-center">Individual Coaching Program</h2>
-          <h3 className="text-xl font-medium">The details:</h3>
-          <ul className="list-disc pl-6 space-y-2 text-lg">
+        <div className="max-w-screen-xl mx-auto space-y-6">
+          <h2 className="text-[45px] font-semibold">Individual Coaching Program</h2>
+          <h3 className="text-[35px] font-medium">The details:</h3>
+          <ul className="list-disc pl-6 space-y-2 text-[23px]">
             <li>
               Work with a certified coach to <strong>significantly improve</strong> your life and relationships
             </li>
