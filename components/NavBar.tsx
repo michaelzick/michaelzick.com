ecf73f2a49db03ae5410228c64b3d8be9c247590
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-08-12 12:02:14 -0700

    Fix mobile drawer layout and link clickability

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -36,7 +36,7 @@ export default function NavBar() {
       <nav
         className={`relative z-50 flex w-full items-center justify-between text-white transition-all duration-500 ${
           scrolled ? 'py-4' : 'py-8'
-        }`}
+        } ${menuOpen ? 'pointer-events-none' : ''}`}
       >
         <Link href="/" className={`${menuOpen ? '!text-default-grey' : ''}`}>
           <h1
@@ -89,7 +89,7 @@ export default function NavBar() {
             type="button"
             aria-label="Toggle menu"
             aria-expanded={menuOpen}
-            className="header-burger-btn burger hidden max-[929px]:block"
+            className="header-burger-btn burger hidden max-[929px]:block pointer-events-auto"
             data-test="header-burger"
             onClick={() => setMenuOpen(!menuOpen)}
           >
@@ -107,7 +107,7 @@ export default function NavBar() {
         </div>
       </nav>
         <div
-          className={`fixed inset-0 bg-white text-default-grey z-40 hidden max-[929px]:flex flex-col items-end justify-start pt-24 space-y-6 p-8 transform transition-transform duration-300 ${
+          className={`fixed inset-0 h-screen bg-white text-default-grey z-40 hidden max-[929px]:flex flex-col items-end justify-start pt-24 space-y-6 p-8 transform transition-transform duration-300 ${
             menuOpen ? 'translate-x-0' : 'translate-x-full pointer-events-none'
           }`}
         >
