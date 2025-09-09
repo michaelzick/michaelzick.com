c6183894c5612c64ed5d67e5a2736d9464a0310b
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-09-09 16:51:40 -0700

    replaced export with standalone (working!)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: 'standalone',
   images: {
     domains: ['www.michaelzick.com', 'michaelzick.com'],
   },
