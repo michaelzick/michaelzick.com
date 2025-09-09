76a6131f96177123b186a6c59884503ca5558a22
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-09-09 16:40:29 -0700

    added output to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     domains: ['www.michaelzick.com', 'michaelzick.com'],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
