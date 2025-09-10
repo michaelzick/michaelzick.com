e5b84bb24e18c3473009bdb94a0fd61b744105c3
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-09-09 17:24:14 -0700

    changed next.config.ts to .js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   output: 'standalone',
   images: {
     domains: ['www.michaelzick.com', 'michaelzick.com'],
