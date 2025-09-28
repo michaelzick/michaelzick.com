64013ddadd9ad979369a91b30d4b3654e919faa1
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2025-09-28 09:55:31 -0700

    left aligned Individual Coaching Program content

--- app/page.tsx
+++ app/page.tsx
@@ -137,6 +137,35 @@ export default function Home() {
         </div>
       </section>
 
+      <section className="px-6 md:px-8 py-24 md:py-32 bg-[rgb(var(--light-grey))] text-default-grey">
+        <div className="max-w-[1400px] mx-auto md:flex md:justify-end">
+          <div className="space-y-6 md:w-1/2 md:flex md:flex-col md:items-start">
+            <h2 className="text-[45px] font-semibold">Individual Coaching Program</h2>
+            <h3 className="text-[35px] font-medium">The details:</h3>
+            <ul className="list-disc pl-6 space-y-2 text-[23px]">
+              <li>
+                Work with a certified coach to <strong>significantly improve</strong> your life and relationships
+              </li>
+              <li>
+                <strong>Powerful</strong> homework assignments that dive deep; we’ll leave no stone unturned
+              </li>
+              <li>
+                <strong>Unlimited emails</strong> between sessions (subject to availability)
+              </li>
+              <li>6-month program to start; variable after that</li>
+              <li>Free Belief Reprogramming Workbook</li>
+            </ul>
+            <a
+              href="https://calendly.com/michaelzick/45min"
+              target="_blank"
+              className="btn mt-6 inline-block text-lg"
+            >
+              Book a Free 45-Minute Strategy Session
+            </a>
+          </div>
+        </div>
+      </section>
+
       {/* Newsletter Section */}
       {/* <section className="py-24 bg-gray-800 text-white">
         <div className="max-w-3xl mx-auto space-y-4">
