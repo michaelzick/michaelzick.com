9de1288a880080fb2ce9abd5255c93858810f5af
Michael Zick <1907006+michaelzick@users.noreply.github.com>
2026-03-02 14:21:12 -0800

    feat: Add web app manifest and favicon images for improved branding and user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,6 +103,19 @@ export const metadata: Metadata = {
     description: siteConfig.description,
     images: [siteConfig.defaultImage],
   },
+  manifest: '/site.webmanifest',
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: '64x64' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }],
+    other: [
+      { rel: 'icon', url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { rel: 'icon', url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+  },
   robots: {
     index: true,
     follow: true,
